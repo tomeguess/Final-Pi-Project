e10a4f763390ef476842079b3d0e65ad81bfe3b6
collyngv <165182014+collyngv@users.noreply.github.com>
2024-04-27 13:31:24 -0500

    Create plant.py
    will eventually change the plant

---
+++ plant.py
@@ -0,0 +1,12 @@
+# a file that allows the gui to store different plant information 
+# and allows the user to permanently add a plant to the list
+
+# create an empty list of plant dictionaries
+ListofPlants = []
+plantName = input("What plant would yould you like to change to? ")
+if plantName in listofPlants:
+  # set plant
+else:
+  plantName = {'ideal temperature': 'input("What is the ideal temerature for this plant? ")'}
+
+
