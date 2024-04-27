a3d30ea743ca7a547a5bb5c565a9355d5bd2c0d8
collyngv <165182014+collyngv@users.noreply.github.com>
2024-04-27 13:32:13 -0500

    Update GUI setup.py
    added the button

--- GUI setup.py
+++ GUI setup.py
@@ -8,73 +8,72 @@ class Greenhouse(Frame):
         # initialization
         self.layout()
 
-    # generalize width and height for all boxes
+    # generalize width, height, and color for all boxes
     WIDTH = 43
     HEIGHT = 7
+    COLOR = "snow2"
+    FONT = "Georgia"
     def layout(self):
         # create label for temperature display
         self.display = Label(self, text = "Current Greenhouse Temperature",
-                             anchor = N, bg = "white",
+                             anchor = N, bg = self.COLOR,
                              height = self.HEIGHT, width = self.WIDTH,
-                             font = ("Arial", 20))
+                             font = (self.FONT, 20))
         # create grid manager for label
         self.display.grid(row = 0, column = 0, columnspan = 3,\
                           sticky = E+W+N+S)
         
         # create label for Humidity display
         self.display = Label(self, text = "Current Greenhouse Humidity",
-                             anchor = N, bg = "white",
+                             anchor = N, bg = self.COLOR,
                              height = self.HEIGHT, width = self.WIDTH,
-                             font = ("Arial", 20))
+                             font = (self.FONT, 20))
         # create grid manager for label
         self.display.grid(row = 0, column = 3, columnspan = 3,\
                           sticky = E+W+N+S)
         
         # create label for ideal environment display
         self.display = Label(self, text = "Ideal Plant environment",
-                             anchor = N, bg = "white",
+                             anchor = N, bg = self.COLOR,
                              height = self.HEIGHT, width = self.WIDTH,
-                             font = ("Arial", 20))
+                             font = (self.FONT, 20))
         # create grid manager for label
         self.display.grid(row = 1, column = 0, columnspan = 3,\
                           sticky = E+W+N+S)
         
         # create label for soil moisuture display
         self.display = Label(self, text = "Current Soil Moisture",
-                             anchor = N, bg = "white",
+                             anchor = N, bg = self.COLOR,
                              height = self.HEIGHT, width = self.WIDTH,
-                             font = ("Arial", 20))
+                             font = (self.FONT, 20))
         # create grid manager for label
         self.display.grid(row = 1, column = 3, columnspan = 3,\
                           sticky = E+W+N+S)
         
         # create label for fan and mister status
         self.display = Label(self, text = "Status of Greenhouse Regulators",
-                             anchor = N, bg = "white",
+                             anchor = N, bg = self.COLOR,
                              height = self.HEIGHT, width = self.WIDTH,
-                             font = ("Arial", 20))
+                             font = (self.FONT, 20))
         # create grid manager for label
         self.display.grid(row = 2, column = 3, columnspan = 3,\
                           sticky = E+W+N+S)
         self.pack()
 
-'''
         # button creation
         # create button to change the plant that is in the greenhouse
-        img = PhotoImage(file = "add.gif")
-        button = Button(self, bg = "white", image = img,\
-                        command = lambda:self.process("+"))
+        img = PhotoImage(file = "change plant button.PNG")
+        button = Button(self, bg = "white", image = img)
         button.image = img
-        button.grid(row = 3, column = 0, columnspan = 3,\
+        button.grid(row = 2, column = 0, columnspan = 3,\
                     sticky = N+S+E+W)
 
         # pack the buttons
         self.pack(fill = BOTH, expand = 1)
-'''
 
 
 # some behavior
 window = Tk() # creates a Tk object
-window.title("The Smart Greenhouse")
+window.title("The Plant Palace")
 g = Greenhouse (window)
 window.mainloop()
