807e3680d592bd330f13e427c63129274863cd7a
collyngv <165182014+collyngv@users.noreply.github.com>
2024-05-01 14:29:13 -0500

    Update plant.py
    not sure if this is needed or not

--- plant.py
+++ plant.py
@@ -1,12 +1,27 @@
 # a file that allows the gui to store different plant information 
 # and allows the user to permanently add a plant to the list
 
-# create an empty list of plant dictionaries
-ListofPlants = []
-plantName = input("What plant would yould you like to change to? ")
-if plantName in listofPlants:
-  # set plant
-else:
-  plantName = {'ideal temperature': 'input("What is the ideal temerature for this plant? ")'}
+# Dictionary to store plant information
+plants= {}
+
+def changePlant(plants):
+
+    # Ask user for the plant name
+    plantName = input("Enter the name of the plant you would like to use: ")
+
+    # Check if the plant already exists in the dictionary
+    if plantName in plants:
+        return plants
+    else:
+        # Ask for ideal temperature, soil moisture level, and humidity
+        idealTemperature = input("Enter the ideal temperature for the plant: ")
+        soilMoisture = input("Enter the ideal soil moisture level for the plant: ")
+        idealHumidity = input("Enter the ideal humidity for the plant: ")
+
+        # Store plant information in the dictionary
+        plants[plantName] = {'idealTemperature': idealTemperature,
+                             'soilMoisture': soilMoisture,
+                             'humidity': idealHumidity}
+        return plants
 
 
